b774134e00db6dcdc902aa9877db4fd39e90972d
demeb324 <demeb324@gmail.com>
2025-06-25 22:06:14 -0600

    fixed backend and connected to cloud database

--- backend/aggiefindit/aggiefindit/wsgi.py
+++ backend/aggiefindit/aggiefindit/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Aggie_FindIt project.
+WSGI config for aggiefindit project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Aggie_FindIt.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aggiefindit.settings')
 
 application = get_wsgi_application()
